02b594316651404d1287a89398e367eacfc11c52
Name <user@example.com>
2024-11-07 15:01:23 +0700

    adding some feat

--- src/component/secondpage.jsx
+++ src/component/secondpage.jsx
@@ -1,32 +1,57 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-function SecondPage() {
+const App = () => {
   const [question, setQuestion] = useState([]);
-
   const api = "https://opentdb.com/api.php?amount=5&type=multiple";
 
   useEffect(() => {
     const fetchData = async () => {
-      const responses = await axios.get(api);
-      const data = responses.data.results;
-      setQuestion(data.map((item) => item.question));
-    };
-    fetchData();
-  });
+      try {
+        const responses = await axios.get(api);
+        const data = responses.data.results;
 
-  console.log(question);
+        setQuestion(
+          data.map((item) => {
+            const allAnswers = [
+              item.correct_answer,
+              ...item.incorrect_answers,
+            ].sort(() => Math.random() - 0.5);
+
+            return {
+              ...item,
+              shuffledAnswer: allAnswers,
+            };
+          })
+        );
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="w-2/5 h-4/5 flex flex-col">
-      {question.map((item, i) => (
-        <p className="text-left mt-11" key={i}>
-          {item}
-        </p>
+    <div>
+      {question.map((q, index) => (
+        <div key={index}>
+          <h3>{q.question}</h3>
+          <div className="flex flex-row">
+            {q.shuffledAnswer.map((answer, i) => (
+              <p className="gap-2 m-3 p-5 rounded-md border-2" key={i}>
+                {answer}
+              </p>
+            ))}
+          </div>
+        </div>
       ))}
+      <div className="flex flex-row justify-center items-center gap-6">
+        <p>{`You scored 0 of ${question.length}`} </p>
+        <button className="bg-blue-400 rounded p-2">Submit</button>
+      </div>
     </div>
   );
-}
+};
 
-export default SecondPage;
+export default App;
